ee5c20d042596f86f9b765909909beffb6193773
Gabriel Raposo <gabriel.robertor@gmail.com>
2023-06-19 12:58:17 -0700

    chore(dpk): refactors code to be more concise and clean while maintaning the same function

--- dpk.js
+++ dpk.js
@@ -3,28 +3,30 @@ const crypto = require('crypto');
 exports.deterministicPartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = '0';
   const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash('sha3-512').update(data).digest('hex');
-    }
+  if (!event) {
+    return TRIVIAL_PARTITION_KEY;
   }
 
-  if (candidate) {
-    if (typeof candidate !== 'string') {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
-  }
+  let candidate = determineCadidate(event);
 
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash('sha3-512').update(candidate).digest('hex');
+    candidate = hashData(candidate);
   }
 
   return candidate;
+};
+
+const determineCadidate = (event) => {
+  let candidate = event.partitionKey || hashData(JSON.stringify(event));
+
+  if (typeof candidate !== 'string') {
+    candidate = JSON.stringify(candidate);
+  }
+
+  return candidate;
+};
+
+const hashData = (data) => {
+  return crypto.createHash('sha3-512').update(data).digest('hex');
 };
